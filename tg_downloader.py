c8651abe91df75cfec8edc5f863497fc801c532d
rodriguezst <carlos@rodriguezst.es>
2019-02-11 19:38:51 +0100

    Start/stop messages and DEBUG_ENABLED env var
    Start/stop messages are printed to console
    Only print to console full update object when env variable DEBUG_ENABLED is set to '1'

--- tg_downloader.py
+++ tg_downloader.py
@@ -29,13 +29,14 @@ api_id = get_env('TG_API_ID', 'Enter your API ID: ', int)
 api_hash = get_env('TG_API_HASH', 'Enter your API hash: ')
 bot_token = get_env('TG_BOT_TOKEN', 'Enter your Telegram BOT token: ')
 download_path = os.environ.get('TG_DOWNLOAD_PATH', '/downloads')
+debug_enabled = os.environ.get('DEBUG_ENABLED', 0, int)
 proxy = None  # https://github.com/Anorov/PySocks
 
 client = TelegramClient(session, api_id, api_hash, proxy=proxy)
 
 # This is our update handler. It is called when a new update arrives.
 async def handler(update):
-    print(update)
+    if(debug_enabled == 1) print(update)
     if update.message.media is not None:
         print("Download started at %s" % (time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())))
         download_result = await client.download_media(update.message, download_path)
@@ -50,8 +51,9 @@ try:
     client.add_event_handler(handler)
 
     # Run the client until Ctrl+C is pressed, or the client disconnects
-    print('(Press Ctrl+C to stop this)')
+    print('Successfully started (Press Ctrl+C to stop)')
     client.run_until_disconnected()
 finally:
     client.disconnect()
+    print('Stopped!')
     
