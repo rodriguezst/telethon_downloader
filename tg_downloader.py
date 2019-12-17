41c3b810f52a489cc04128b76b8c68c3598550a1
rodriguezst <carlos@rodriguezst.es>
2019-12-17 20:08:59 +0100

    Few minor changes
    Added optional TG_MAX_PARALLEL env variable to edit the maximum number of parallel downloads
    Added optional TG_DL_TIMEOUT env variable to edit the maximum time allowed for a file download
    Implemented error detection to inform the sender when a file could not be downloaded
    Files are now saved to a temp directory until the download is completed and moved afterwards
    Removed chown and associated env variables

--- tg_downloader.py
+++ tg_downloader.py
@@ -6,6 +6,12 @@ import asyncio
 
 # Import the client
 from telethon import TelegramClient, events
+from telethon.tl import types
+
+# Enable logging
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.DEBUG)
 
 # This is a helper method to access environment variables or
 # prompt the user to type them in the terminal if missing.
@@ -27,14 +33,17 @@ api_hash = get_env('TG_API_HASH', 'Enter your API hash: ')
 bot_token = get_env('TG_BOT_TOKEN', 'Enter your Telegram BOT token: ')
 download_path = get_env('TG_DOWNLOAD_PATH', 'Enter full path to downloads directory: ')
 debug_enabled = ('DEBUG_ENABLED' in os.environ)
-user_id = get_env('USER_ID', 'Enter the id of the user to chown downloaded files to: ', int)
-group_id = get_env('GROUP_ID', 'Enter the id of the group to chown downloaded files to: ', int)
 
-number_of_parallel_downloads = 4
+number_of_parallel_downloads = int(os.environ.get('TG_MAX_PARALLEL',4))
+maximum_seconds_per_download = int(os.environ.get('TG_DL_TIMEOUT',3600))
 proxy = None  # https://github.com/Anorov/PySocks
 
 #Create a queue that we will use to store our downloads.
 queue = asyncio.Queue()
+
+#Create tmp path to store downloads until completed
+tmp_path = os.path.join(download_path,"tmp")
+os.makedirs(tmp_path, exist_ok=True)
 
 async def worker(name):
     while True:
@@ -43,18 +52,42 @@ async def worker(name):
         update = queue_item[0]
         message = queue_item[1]
 
+        file_path = tmp_path;
+        file_name = 'unknown name';
+        attributes = update.message.media.document.attributes
+        for attr in attributes:
+            if isinstance(attr, types.DocumentAttributeFilename):
+                file_name = attr.file_name
+                file_path = os.path.join(file_path, attr.file_name)
         await message.edit('Downloading...')
-        print("Download started at %s" % (time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())))
-        download_result = await client.download_media(update.message, download_path)
-        end_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-        print("Finished downloading file %s at %s" % (download_result,end_time))
-        os.chown(download_result, int(user_id), int(group_id))
-        await message.edit('Finished!')
+        print("[%s] Download started at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        try:
+            loop = asyncio.get_event_loop()
+            task = loop.create_task(client.download_media(update.message, file_path))
+            download_result = await asyncio.wait_for(task, timeout=maximum_seconds_per_download)
+            end_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            end_time_short = time.strftime("%H:%M", time.localtime())
+            os.chown(download_result, int(user_id), int(group_id))
+            _,filename = os.path.split(download_result)
+            final_path = os.path.join(download_path,filename)
+            os.rename(download_result,final_path)
+            print("[%s] Successfully downloaded to %s at %s" % (file_name, final_path, end_time))
+            await message.edit('Finished at %s' %(end_time_short))
+        except asyncio.TimeoutError:
+            print("[%s] Timeout reached at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+            await message.edit('Error!')
+            message = await update.reply('ERROR: Timeout reached downloading this file')
+        except Exception as e:
+            print("[EXCEPTION]: %s" % (str(e)))
+            #print("[%s]: %s" % (e.__class__.__name__, str(e)))
+            print("[%s] Exception at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+            await message.edit('Error!')
+            message = await update.reply('ERROR: Exception %s raised downloading this file' % (e.__class__.__name__))
 
         # Notify the queue that the "work item" has been processed.
         queue.task_done()
 
-client = TelegramClient(session, api_id, api_hash, proxy=proxy)
+client = TelegramClient(session, api_id, api_hash, proxy=proxy, request_retries=10, flood_sleep_threshold=120)
 
 # This is our update handler. It is called when a new update arrives.
 # Register `events.NewMessage` before defining the client.
@@ -63,7 +96,12 @@ async def handler(update):
     if(debug_enabled):
         print(update)
     if update.message.media is not None:
-        print("Download queued at %s" % (time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())))
+        file_name = 'unknown name';
+        attributes = update.message.media.document.attributes
+        for attr in attributes:
+            if isinstance(attr, types.DocumentAttributeFilename):
+                file_name = attr.file_name
+        print("[%s] Download queued at %s" % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
         message = await update.reply('In queue')
         await queue.put([update, message])
 
